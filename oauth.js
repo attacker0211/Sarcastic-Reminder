da94d125a2e69a1a0e0786539901fdd6c2f41fce
Tien Phan <attacker0211@gmail.com>
2019-03-03 05:28:43 -0800

    add notification js

--- oauth.js
+++ oauth.js
@@ -1,5 +1,6 @@
 window.onload = function() {
   // document.querySelector('button').addEventListener('click', function() {
+
     chrome.identity.getAuthToken({interactive: true}, function(token) {
       let init = {
         method: 'GET',
