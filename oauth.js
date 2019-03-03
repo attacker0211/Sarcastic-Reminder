ceeb584b2abc603d6a8ee73e206a12a846ff3cb4
Tien Phan <attacker0211@gmail.com>
2019-03-02 18:13:11 -0800

    delete dumb comments

--- oauth.js
+++ oauth.js
@@ -1,23 +1,3 @@
-// window.onload = function() {
-//   document.querySelector('button').addEventListener('click', function() {
-//     chrome.identity.getAuthToken({interactive: true}, function(token) {
-//       let init = {
-//         method: 'GET',
-//         async: true,
-//         headers: {
-//           Authorization: 'Bearer ' + token,
-//           'Content-Type': 'application/json'
-//         }
-//       }
-//       fetch(
-//           'https://www.googleapis.com/calendar/v3/calendars/primary/events?key=AIzaSyA3Ph01ZChAs9Tr5Fo19XTT38OsZxU4yzw', init)
-//           .then((response) => response.json())
-//           .then(function(data) {
-//             console.log(data)
-//           });
-//     });
-//   });
-// };
 window.onload = function() {
   document.querySelector('button').addEventListener('click', function() {
     chrome.identity.getAuthToken({interactive: true}, function(token) {
