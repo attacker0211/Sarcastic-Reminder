c9d94660160bfc6dd078fbaf3240df21871c1f68
Tien Phan <attacker0211@gmail.com>
2019-03-02 18:10:53 -0800

    calendar retreiver

--- oauth.js
+++ oauth.js
@@ -1,7 +1,42 @@
+// window.onload = function() {
+//   document.querySelector('button').addEventListener('click', function() {
+//     chrome.identity.getAuthToken({interactive: true}, function(token) {
+//       let init = {
+//         method: 'GET',
+//         async: true,
+//         headers: {
+//           Authorization: 'Bearer ' + token,
+//           'Content-Type': 'application/json'
+//         }
+//       }
+//       fetch(
+//           'https://www.googleapis.com/calendar/v3/calendars/primary/events?key=AIzaSyA3Ph01ZChAs9Tr5Fo19XTT38OsZxU4yzw', init)
+//           .then((response) => response.json())
+//           .then(function(data) {
+//             console.log(data)
+//           });
+//     });
+//   });
+// };
 window.onload = function() {
   document.querySelector('button').addEventListener('click', function() {
     chrome.identity.getAuthToken({interactive: true}, function(token) {
-      console.log(token);
+      let init = {
+        method: 'GET',
+        async: true,
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'application/json'
+        },
+        'contentType': 'json'
+      };
+      fetch(
+          'https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMax=2019-03-03T12%3A00%3A00Z&timeMin=2019-03-01T12%3A00%3A00Z&key=AIzaSyA3Ph01ZChAs9Tr5Fo19XTT38OsZxU4yzw',
+          init)
+          .then((response) => response.json())
+          .then(function(data) {
+            console.log(data)
+          });
     });
   });
 };
